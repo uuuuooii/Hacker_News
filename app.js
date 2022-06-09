5d6679c41143acc189ce9f5455715e4bd6fb25ca
neo@zillinks.com <keunhey0107@gmail.com>
2022-06-09 20:38:23 +0900

    페이징 구현

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const store = {
+    currentPage:  1,
+};
 
 function getData(url) {
     ajax.open('GET', url, false);
@@ -12,14 +15,15 @@ function getData(url) {
 }
 
 function newsFeed() {
-const newsFeed = getData(NEWS_URL)
-const newsList = [];
+    const newsFeed = getData(NEWS_URL);
+    const newsList = [];
 
-newsList.push('<ul>')
-for(let i = 0; i < 10; i++){  
+newsList.push('<ul>');
+
+for(let i = (store.currentPage -1) * 10; i < store.currentPage *10; i++){  
     newsList.push(`
         <li>
-            <a href="#${newsFeed[i].id}">
+            <a href="#/show/${newsFeed[i].id}">
             ${newsFeed[i].title} (${newsFeed[i].comments_count})
             </a>
         </li>
@@ -27,25 +31,27 @@ for(let i = 0; i < 10; i++){
 }
 
 newsList.push('</ul>');
+newsList.push(`
+    <div>
+        <a href="#/page/${store.currentPage > 1 ? store.currentPage -1 : 1}">이전 페이지</a>
+        <a href="#/page/${store.currentPage +1}">다음 페이지</a>
+    </div>
+`);
 
 container.innerHTML = newsList.join('');
-
- 
 }
 
-const ul = document.createElement('ul');
-
 function newsDetail() {
-    const id = location.hash.substr(1);
-    const newsContent = getData(CONTENT_URL.replace('@id',id))
+const id = location.hash.substr(7);
+const newsContent = getData(CONTENT_URL.replace('@id', id))
 
     container.innerHTML = `
-    <h1>${newsContent.title}</h1>
+        <h1>${newsContent.title}</h1>
 
-    <div>
-        <a href="#">목록으로</a>
-    </div>
-    `
+        <div>
+            <a href="#/page/${store.currentPage}">목록으로</a>
+        </div>
+        `
 }
 
 function router() {
@@ -53,8 +59,11 @@ function router() {
 
     if (routePath === '') {
         newsFeed();
+    } else if (routePath.indexOf('#/page/') >= 0){
+        store.currentPage = Number(routePath.substr(7));
+        newsFeed();  
     } else {
-        newsDetail();  
+        newsDetail(); 
     }
 }
 
