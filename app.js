377909b377e5d4e5cbfaf15f37053716026aae60
neo@zillinks.com <keunhey0107@gmail.com>
2022-06-15 18:34:34 +0900

    ux 개선

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 const store = {
     currentPage: 1,
+    feeds: [],
 };
 
 function getData(url) {
@@ -14,9 +15,18 @@ function getData(url) {
     return JSON.parse(ajax.response);
 }
 
+function makeFeeds(feeds) {
+    for(let i = 0; i < feeds.length; i++) {
+        feeds[i].read = false;
+    }
+
+    return feeds;
+}
+
+
 // Feed content
 function newsFeed() {
-    const newsFeed = getData(NEWS_URL);
+    let newsFeed = getData(NEWS_URL);
     const newsList = [];
     let template = `
         <div class="bg-gray-600 min-h-screen">
@@ -41,11 +51,15 @@ function newsFeed() {
                 {{__news_feed__}}        
             </div>
         </div>
-    `
+    `;
+
+    if (newsFeed.length === 0) {
+        newsFeed = store.feeds = makefeeds(getData(NEWS_URL));
+    }
 // news data
     for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
         newsList.push(`
-        <div class="p-6 ${newsFeed[i].read ? 'bg-red-500' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+        <div class="p-6 ${newsFeed[i].read ? 'bg-green-500' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
         <div class="flex">
             <div class="flex-auto">
                 <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>  
@@ -104,6 +118,14 @@ function newsDetail() {
     </div>
   </div>
     `;
+
+
+    for (let i = 0; i < store.feeds.length; i++) {
+        if (store.feeds[i].id === Number(id)) {
+            store.feeds[i].read = true;
+            break;
+        }
+    }
 
     function makeComment(comments, called = 0) {
         const commentString = [];
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -124,23 +124,37 @@ var content = document.createElement('div');
 var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 var CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 var store = {
-  currentPage: 1
+  currentPage: 1,
+  feeds: []
 };
 
 function getData(url) {
   ajax.open('GET', url, false);
   ajax.send();
   return JSON.parse(ajax.response);
+}
+
+function makeFeeds(feeds) {
+  for (var i = 0; i < feeds.length; i++) {
+    feeds[i].read = false;
+  }
+
+  return feeds;
 } // Feed content
 
 
 function newsFeed() {
   var newsFeed = getData(NEWS_URL);
   var newsList = [];
-  var template = "\n        <div class=\"bg-gray-600 min-h-screen\">\n            <div class=\"bg-white text-xl\">\n                <div class=\"mx-auto px-4\">\n                    <div class=\"flex justify-between items-center py-6\">\n                        <div class=\"flex justify-start\">\n                            <h1 class=\"font-extrabold\">Hacker News</h1>\n                        </div>\n                        <div class=\"items-center justify-end\">\n                            <a href=\"#/page/{{__prev_page__}}\" class=\"text-gray-500\">\n                                Previous\n                            </a>\n                            <a href=\"#/page/{{__next_page__}}\" class=\"text-gray-500 ml-4\">\n                                Next\n                            </a>\n                        </div>\n                    </div> \n                </div>\n            </div>\n            <div class=\"p-4 text-2xl text-gray-700\">\n                {{__news_feed__}}        \n            </div>\n        </div>\n    "; // news data
+  var template = "\n        <div class=\"bg-gray-600 min-h-screen\">\n            <div class=\"bg-white text-xl\">\n                <div class=\"mx-auto px-4\">\n                    <div class=\"flex justify-between items-center py-6\">\n                        <div class=\"flex justify-start\">\n                            <h1 class=\"font-extrabold\">Hacker News</h1>\n                        </div>\n                        <div class=\"items-center justify-end\">\n                            <a href=\"#/page/{{__prev_page__}}\" class=\"text-gray-500\">\n                                Previous\n                            </a>\n                            <a href=\"#/page/{{__next_page__}}\" class=\"text-gray-500 ml-4\">\n                                Next\n                            </a>\n                        </div>\n                    </div> \n                </div>\n            </div>\n            <div class=\"p-4 text-2xl text-gray-700\">\n                {{__news_feed__}}        \n            </div>\n        </div>\n    ";
+
+  if (newsFeed.length === 0) {
+    newsFeed = store.feeds = makefeeds(getData(NEWS_URL));
+  } // news data
+
 
   for (var i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
-    newsList.push("\n        <div class=\"p-6 ".concat(newsFeed[i].read ? 'bg-red-500' : 'bg-white', " mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100\">\n        <div class=\"flex\">\n            <div class=\"flex-auto\">\n                <a href=\"#/show/").concat(newsFeed[i].id, "\">").concat(newsFeed[i].title, "</a>  \n            </div>\n            <div class=\"text-center text-sm\">\n                <div class=\"w-10 text-white bg-green-300 rounded-lg px-0 py-2\">").concat(newsFeed[i].comments_count, "</div>\n            </div>\n        </div>\n        <div class=\"flex mt-3\">\n            <div class=\"grid grid-cols-3 text-sm text-gray-500\">\n                <div><i class=\"fas fa-user mr-1\"></i>").concat(newsFeed[i].user, "</div>\n                <div><i class=\"fas fa-heart mr-1\"></i>").concat(newsFeed[i].points, "</div>\n                <div><i class=\"far fa-clock mr-1\"></i>").concat(newsFeed[i].time_ago, "</div>\n            </div>  \n        </div>\n    </div>\n    "));
+    newsList.push("\n        <div class=\"p-6 ".concat(newsFeed[i].read ? 'bg-green-500' : 'bg-white', " mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100\">\n        <div class=\"flex\">\n            <div class=\"flex-auto\">\n                <a href=\"#/show/").concat(newsFeed[i].id, "\">").concat(newsFeed[i].title, "</a>  \n            </div>\n            <div class=\"text-center text-sm\">\n                <div class=\"w-10 text-white bg-green-300 rounded-lg px-0 py-2\">").concat(newsFeed[i].comments_count, "</div>\n            </div>\n        </div>\n        <div class=\"flex mt-3\">\n            <div class=\"grid grid-cols-3 text-sm text-gray-500\">\n                <div><i class=\"fas fa-user mr-1\"></i>").concat(newsFeed[i].user, "</div>\n                <div><i class=\"fas fa-heart mr-1\"></i>").concat(newsFeed[i].points, "</div>\n                <div><i class=\"far fa-clock mr-1\"></i>").concat(newsFeed[i].time_ago, "</div>\n            </div>  \n        </div>\n    </div>\n    "));
   }
 
   template = template.replace('{{__news_feed__}}', newsList.join(''));
@@ -155,15 +169,22 @@ function newsDetail() {
   var newsContent = getData(CONTENT_URL.replace('@id', id));
   var template = "\n    <div class=\"bg-gray-600 min-h-screen pb-8\">\n    <div class=\"bg-white text-xl\">\n        <div class=\"mx-auto px-4\">\n            <div class=\"flex justify-between items-center py-6\">\n                <div class=\"flex justify-start\">\n                    <h1 class=\"font-extrabold\">Hacker News</h1>\n                </div>\n                <div class=\"items-center justify-end\">\n                    <a href=\"#/page/".concat(store.currentPage, "\" class=\"text-gray-500\">\n                        <i class=\"fa fa-times\"></i>\n                    </a>\n                </div>\n            </div>\n        </div>\n    </div>\n\n    <div class=\"h-full border rounded-xl bg-white m-6 p-4 \">\n      <h2>").concat(newsContent.title, "</h2>\n      <div class=\"text-gray-400 h-20\">\n        ").concat(newsContent.content, "\n      </div>\n\n      {{__comments__}}\n\n    </div>\n  </div>\n    ");
 
+  for (var i = 0; i < store.feeds.length; i++) {
+    if (store.feeds[i].id === Number(id)) {
+      store.feeds[i].read = true;
+      break;
+    }
+  }
+
   function makeComment(comments) {
     var called = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
     var commentString = [];
 
-    for (var i = 0; i < comments.length; i++) {
-      commentString.push("\n                <div style=\"padding-left: ".concat(called * 40, "px;\" class=\"mt-4\">\n                    <div class=\"text-gray-400\">\n                        <i class=\"fa fa-sort-up mr-2\"></i>\n                        <strong>").concat(comments[i].user, "</strong> ").concat(comments[i].time_ago, "\n                    </div>\n                    <p class=\"text-gray-700\">").concat(comments[i].content, "</p>\n                </div>\n            "));
+    for (var _i = 0; _i < comments.length; _i++) {
+      commentString.push("\n                <div style=\"padding-left: ".concat(called * 40, "px;\" class=\"mt-4\">\n                    <div class=\"text-gray-400\">\n                        <i class=\"fa fa-sort-up mr-2\"></i>\n                        <strong>").concat(comments[_i].user, "</strong> ").concat(comments[_i].time_ago, "\n                    </div>\n                    <p class=\"text-gray-700\">").concat(comments[_i].content, "</p>\n                </div>\n            "));
 
-      if (comments[i].comments.length > 0) {
-        commentString.push(makeComment(comments[i].comments, called + 1));
+      if (comments[_i].comments.length > 0) {
+        commentString.push(makeComment(comments[_i].comments, called + 1));
       }
     }
 
@@ -216,7 +237,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49491" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51573" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
